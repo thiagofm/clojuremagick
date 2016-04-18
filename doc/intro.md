19815be1c3d37d0a49fdf7d571227626444b5923
Thiago Fernandes Massa <thiagown@gmail.com>
2016-04-18 23:18:44 +0200

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojuremagick
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojuremagick/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojuremagick.core-test
+  (:require [clojure.test :refer :all]
+            [clojuremagick.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
