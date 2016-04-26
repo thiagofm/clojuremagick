af40382fb029e7f34528b9c4fdbaad6f63d79acb
Thiago Fernandes Massa <thiagown@gmail.com>
2016-04-26 23:25:49 +0200

    Added shell dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojuremagick "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Image conversion made simple"
+  :url "http://github.com/thiagofm/clojuremagick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [me.raynes/conch "0.8.0"]])
