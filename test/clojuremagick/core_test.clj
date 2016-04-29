9d793c857ca904d40861c387c1c6d3b9bec2bc92
Thiago Fernandes Massa <thiagown@gmail.com>
2016-04-30 00:37:21 +0200

    Improved failing test

--- test/clojuremagick/core_test.clj
+++ test/clojuremagick/core_test.clj
@@ -3,5 +3,14 @@
             [midje.sweet :as midje])
   (:use midje.sweet))
 
-(facts "about with-file"
-  (cm/with-file "rose_full.jpg" [[:resize "50x50"]]) => (clojure.java.io/file "rose_thumb.jpg"))
+
+
+(with-state-changes [(before :facts
+                             (do
+                               (def rose-temp-file (java.io.File/createTempFile "temp_rose" ".jpg"))
+                               (clojure.java.io/copy (clojure.java.io/file "test/resources/rose_full.jpg") rose-temp-file)))]
+  (facts "about with-file"
+    (.length (cm/with-file (.toString rose-temp-file) [[:resize "100x100"]])) => (.length (clojure.java.io/file "test/resources/rose_thumb.jpg"))))
+;;
+
+;
