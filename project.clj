371e66eef10a2ddbfcd23e8fe849fae710f97807
Thiago Fernandes Massa <thiagown@gmail.com>
2016-05-01 12:42:32 +0200

    Added commons-io dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.commons/commons-io "1.3.2"]
                  [me.raynes/conch "0.8.0"]]
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:default [:dev]
