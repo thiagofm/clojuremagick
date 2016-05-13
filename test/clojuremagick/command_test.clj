4484337fec7eb7e1a1ca707da9a40906fd72e0ec
Thiago Fernandes Massa <thiagown@gmail.com>
2016-05-13 21:36:31 +0200

    Added spec for composed command

--- test/clojuremagick/command_test.clj
+++ test/clojuremagick/command_test.clj
@@ -6,5 +6,9 @@
 (facts "about vec->shell-vec"
        (command/vec->shell-vec [[:resize "50%"]]) => ["-resize" "50%"]
        (command/vec->shell-vec [[:flip]]) => ["-flip"]
+       (command/vec->shell-vec [[:resize "100x100"]
+                                [:rotate "-37"]
+                                [:auto-orient]
+                                [:scale "10"]]) => ["-resize" "100x100" "-rotate" "-37" "-auto-orient" "-scale" "10"]
        (command/vec->shell-vec [[:wat?]]) => (throws clojure.lang.ExceptionInfo
                                                      #(= :wat? (-> % ex-data :command))))
